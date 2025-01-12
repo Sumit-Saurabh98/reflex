0d6e2ea9dfd31a96e40b1ae35a66809e798f0a1b
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2025-01-12 14:41:23 +0530

    commit

--- src/routes/product.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-
-
-const router = express.Router();
-
-router.get('/all-products', getAllProducts);
-
-export default router;
